7ca32d2d292b5dec168f9f42a7c6576e8500d629
Alan Lawrey <sovereign250@gmail.com>
2015-03-01 17:27:35 +1100

    - Added common file.

---
+++ vot/common.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace vot
+{
+    class Group
+    {
+        public:
+            enum Type
+            {
+                PLAYER,
+                ENEMY,
+                NATURE
+            };
+    };
+}
