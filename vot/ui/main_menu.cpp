b73fbd0e8b896201ef7028f2d5e462eed9dd14dc
Astrellon <sovereign250@gmail.com>
2015-03-26 19:45:35 +1100

    - Left out main menu cpp.

---
+++ vot/ui/main_menu.cpp
@@ -0,0 +1,16 @@
+#include "main_menu.h"
+
+namespace vot
+{
+    namespace ui
+    {
+        MainMenu::MainMenu()
+        {
+
+        }
+        void MainMenu::draw(sf::RenderTarget &target, sf::RenderStates states) const
+        {
+
+        }
+    }
+}
